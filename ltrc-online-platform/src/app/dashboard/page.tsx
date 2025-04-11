367ffaae0e6bb2e767a32bfe110fefa1e28b4e79
Dimmsum <153735532+Dimmsum@users.noreply.github.com>
2025-04-11 14:52:40 +0000

    Setting p different looks

---
+++ ltrc-online-platform/src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import './dashboard.css'
+
+
+
+const dashboard = () =>
+{
+return(
+  <>
+<h1>Dashboard</h1>
+
+<div className = "studentContent">
+  <h3>ID#: </h3>
+  <h3>Name: </h3>
+
+</div>
+</>
+
+
+
+
+
+)
+}
+
+export default dashboard
